3c0da06b807cd5b24f8bd01aa9c1aa9197559eef
hcschneider30 <hcschneider30@hotmail.com>
2014-04-23 21:07:52 +0200

    comment test
    Signed-off-by: hcschneider30 <hcschneider30@hotmail.com>

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## this function will return a vector of functions
 
 makeCacheMatrix <- function(x = matrix()) {
 
